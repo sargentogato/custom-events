fa012506d00f4cbd4ae12f73e5e2e2c47835d8a3
Marlon Velasquez <marlonjvelasquez@gmail.com>
2023-05-07 17:57:49 +0200

    Add config tailwind clean useless files add component and logic

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
-<script></script>
+<template>
+  <div class="flex-col">
+    <div class="flex gap-4 justify-around">
+      <h2>Notes</h2>
+      <p v-for="note in notes" :key="note">
+        {{ note }}
+      </p>
 
-<template></template>
+      <h2>timeStamp</h2>
+      <div v-for="timeStamp in timeStamps">{{ timeStamp }}</div>
+    </div>
+
+    <Input @add-note="addNote" />
+  </div>
+</template>
+
+<script>
+import Input from "./components/Input.vue"
+
+export default {
+  name: "App",
+  components: { Input },
+  data() {
+    return {
+      notes: [],
+      timeStamps: [],
+    }
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note)
+      this.timeStamps.push(event.timeStamp)
+    },
+  },
+}
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <input
+    type="text"
+    class="border"
+    v-model="input"
+    @keyup.enter="monitorEnterKey"
+  />
+</template>
+
+<script>
+export default {
+  name: "Input",
+  emits: ["add-note"],
+  data() {
+    return {
+      input: "",
+    }
+  },
+  methods: {
+    monitorEnterKey() {
+      //código
+      this.$emit("add-note", {
+        note: this.input,
+        timeStamp: new Date().toLocaleString(),
+      })
+
+      this.input = ""
+    },
+  },
+}
+</script>
